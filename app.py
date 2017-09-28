1cc5811d035483f4829384bb65b04cd88de03f6b
Jimmy Mu <jimmymu@klook.com>
2017-09-28 11:08:46 +0800

    add change story status to review

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ app = Flask(__name__)
 TO_DO = 'TO DO'
 IN_PROGRESS = 'IN PROGRESS'
 IN_PROGRESS_STORY = 'Start development'
+IN_REVIEW_STORY='Start testing'
+DONE = 'DONE'
 
 @app.route('/task_story_inprogress',methods=["POST"])
 def task_story_inprogress():
@@ -56,6 +58,33 @@ def subtask_inprogress():
     return 'error'
 
 
+@app.route('/task_done_story_review',methods=["POST"])
+def task_done_story_review():
+    '''
+        当task为done时 如果关联的story所有的task都done 则更改状态为review
+    '''
+    key = request.args.get('issue')
+    jira = connect_jira()
+    task = jira.issue(key)
+    issuelinks = task.fields.issuelinks
+    if len(issuelinks) == 1:
+        story = issuelinks[0]
+        story_key = story.raw.get('outwardIssue',{}).get('key')
+        story = jira.issue(story_key)
+        story_issue_links = story.fields.issuelinks
+        all_done = True
+        for link_task in story_issue_links:
+            if link_task.inwardIssue.fields.name != DONE:
+                all_done = False
+        if all_done:
+             jira.transition_issue(story, transition=IN_REVIEW_STORY)
+            return 'succeed'
+    return 'error'
+
+       
+
+            
+ 
 
 
 def connect_jira():
